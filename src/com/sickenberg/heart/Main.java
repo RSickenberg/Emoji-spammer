39e9e4493f7f1bd38cfe1c43cec369c9fb8857bd
Romain Sickenberg <romain.sickenberg@liip.ch>
2018-01-28 20:56:53 +0100

    Fix

--- src/com/sickenberg/heart/Main.java
+++ src/com/sickenberg/heart/Main.java
@@ -5,12 +5,13 @@ import java.awt.event.KeyEvent;
 
 public class Main {
 
-    private boolean addPeriod = true;
-    private int loop = 300;
-    private String emoji = "heart";
-
     public static void main(String[] args) throws AWTException{
-        heart.init(this.loop, this.emoji, this.addPeriod); // Insert emoji name ❤️
+        boolean addPeriod = false;
+        int loop = 300;
+        String emoji = "heart";
+
+
+        heart.init(loop, emoji, addPeriod); // Insert emoji name ❤️
     }
 
 }
